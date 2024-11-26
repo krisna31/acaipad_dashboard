86339bcaefde826a2de7cd8fc621e8a32fba4f11
krisna31 <krisnaaaputraaa@gmail.com>
2024-11-26 11:03:51 +0700

    try to login in production, still redirect back to login after login

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::redirect('/', '/admin', 301);
+// Route::redirect('/', '/admin', 301);
