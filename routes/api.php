35e180b6d139a868613d2ca13c2b1bc4b05d3817
krisna31 <krisnaaaputraaa@gmail.com>
2024-11-26 16:01:29 +0700

    APi route for add and get daya

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/daya', function (Request $request) {
+    $powers = Power::all();
+
+    return response()->json([
+        'success' => true,
+        'data' => $powers
+    ]);
+})->middleware('auth:sanctum');
+
+Route::post('/daya', function (Request $request) {
+    $validatedRequest = $request->validate([
+        'koneksi' => 'required|in:BLE,WIFI',
+        'daya' => 'required|integer'
+    ]);
+
+    $power = Power::create($validatedRequest);
+
+    return response()->json([
+        'success' => true,
+        'message' => 'Power record created successfully',
+        'data' => $power
+    ], 201);
+});
